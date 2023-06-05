99620db040f49ed5e1671c86e1d9bcfea37cd01c
777warrior <carlos.orb@hotmail.com>
2023-06-05 09:34:37 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider} from './contexts/products.context';
+
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
